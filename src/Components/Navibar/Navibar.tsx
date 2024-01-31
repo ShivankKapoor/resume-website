1ee9ba4225abd523450cea051a27cac223fd3a09
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2024-01-31 16:39:33 -0600

    Underlined when clicked

--- src/Components/Navibar/Navibar.tsx
+++ src/Components/Navibar/Navibar.tsx
@@ -1,8 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Navibar.css";
 import { Nav, Navbar } from "react-bootstrap";
 
 function Navibar() {
+  const location = useLocation().pathname;
 
   return (
     <Navbar bg="light" className="Navigation-Bar">
@@ -10,17 +11,23 @@ function Navibar() {
       <Nav className="me-auto">
         <button type="button" className="btn btn-light">
           <Link to="/" className="nav-link">
-            Home
+            <div className={location === '/' ? 'underline' : ''}>
+              Home
+            </div>
           </Link>
         </button>
         <button type="button" className="btn btn-light">
           <Link to="/resume" className="nav-link">
-            Resume
+            <div className={location === '/resume' ? 'underline' : ''}>
+              Resume
+            </div>
           </Link>
         </button>
         <button type="button" className="btn btn-light">
           <Link to="/projects" className="nav-link">
-            Projects
+            <div className={location === '/projects' ? 'underline' : ''}>
+              Projects
+            </div>
           </Link>
         </button>
       </Nav>
