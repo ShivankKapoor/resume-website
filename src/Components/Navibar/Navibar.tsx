a87e8e5f004121c181720e5959eefa79ab91c2b5
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2024-01-31 16:42:49 -0600

    Better buttons

--- src/Components/Navibar/Navibar.tsx
+++ src/Components/Navibar/Navibar.tsx
@@ -9,27 +9,27 @@ function Navibar() {
     <Navbar bg="light" className="Navigation-Bar">
       <Navbar.Brand className="start" href="/">Shivank Kapoor</Navbar.Brand>
       <Nav className="me-auto">
-        <button type="button" className="btn btn-light">
-          <Link to="/" className="nav-link">
+        <Link to="/" className="nav-link">
+          <button type="button" className="btn btn-light">
             <div className={location === '/' ? 'underline' : ''}>
               Home
             </div>
-          </Link>
-        </button>
-        <button type="button" className="btn btn-light">
-          <Link to="/resume" className="nav-link">
+          </button>
+        </Link>
+        <Link to="/resume" className="nav-link">
+          <button type="button" className="btn btn-light">
             <div className={location === '/resume' ? 'underline' : ''}>
               Resume
             </div>
-          </Link>
-        </button>
-        <button type="button" className="btn btn-light">
-          <Link to="/projects" className="nav-link">
+          </button>
+        </Link>
+        <Link to="/projects" className="nav-link">
+          <button type="button" className="btn btn-light">
             <div className={location === '/projects' ? 'underline' : ''}>
               Projects
             </div>
-          </Link>
-        </button>
+          </button>
+        </Link>
       </Nav>
     </Navbar>
   );
