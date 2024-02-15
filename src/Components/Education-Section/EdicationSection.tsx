f6e249aa481f15f294d8fe7a90092dbeb8254ddc
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2024-02-15 15:17:32 -0600

    Create education title

--- src/Components/Education-Section/EdicationSection.tsx
+++ src/Components/Education-Section/EdicationSection.tsx
@@ -1,1 +1,19 @@
+import { FunctionComponent } from "react";
 import "./EducationSection.css";
+
+type EducationSectionProps = {
+    schoolName:string,
+    schoolLogo:string,
+    degreeTitle:string,
+    degreeMajor:string,
+    yearStart:number,
+    yearEnd:number
+}
+
+export const EducationSection: FunctionComponent<EducationSectionProps> = ({ schoolName, schoolLogo, degreeTitle, degreeMajor, yearStart, yearEnd }) => {
+    return(
+        <></>
+    )
+}
+
+export default EducationSection;
