0b338a1fe17c31b6edafa25a62f36a106dd7b1a8
Shivank <Shivankkapoor@yahoo.com>
2024-02-14 23:56:19 -0600

    Deploy changes

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -4,7 +4,7 @@ import ProfileOverviewCard from "../../Components/Profile-Overview-Card/ProfileO
 function Home() {
   return (
     <>
-      <ProfileOverviewCard firstName="Shivank" lastName="Kapoor" description="
+      <ProfileOverviewCard firstName="Firstname" lastName="Lastname" description="
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
             odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
             quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
