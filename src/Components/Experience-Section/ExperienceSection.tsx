2c04b8f91db6bfb0a0627cf0d8af19b46c19f595
Shivank <Shivankkapoor@yahoo.com>
2024-02-15 21:13:11 -0600

    Add info to experience

--- src/Components/Experience-Section/ExperienceSection.tsx
+++ src/Components/Experience-Section/ExperienceSection.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent } from "react";
 import "./ExperienceSection.css";
 import { Card } from "react-bootstrap";
+import work from "./work.png";
 
 type ExperienceSectionProps = {};
 
@@ -8,13 +9,31 @@ export const ExperienceSection: FunctionComponent<
   ExperienceSectionProps
 > = ({}) => {
   return (
-    <div className="experience-card">
+    <div className="experience-info-card">
       <Card>
         <Card.Header>
           <h3>Experience</h3>
         </Card.Header>
-        <Card.Body>
-            
+        <Card.Body className="flex-row">
+          <div className="experience-image-container">
+            <img className="experience-image" src={work} alt="Education Icon" />
+          </div>
+          <div className="experience-info-container">
+            <h5>Position</h5>
+            <h6>Employer</h6>
+            <h6>MM/YYYY - MM/YYYY</h6>
+            <div className="experience-description">
+              <h6>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </h6>
+            </div>
+          </div>
         </Card.Body>
       </Card>
     </div>
