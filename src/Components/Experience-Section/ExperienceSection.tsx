9a5b5d8b305dd056a73977ed96b97ed5bc1d2aee
Shivank <Shivankkapoor@yahoo.com>
2024-02-15 20:03:17 -0600

    Add Experience Section template files

--- src/Components/Experience-Section/ExperienceSection.tsx
+++ src/Components/Experience-Section/ExperienceSection.tsx
@@ -1,2 +1,16 @@
+import { FunctionComponent } from "react";
 import "./ExperienceSection.css";
 
+type ExperienceSectionProps = {};
+
+export const ExperienceSection: FunctionComponent<
+  ExperienceSectionProps
+> = ({}) => {
+  return (
+    <>
+      <h1>Experience Section Works</h1>
+    </>
+  );
+};
+
+export default ExperienceSection;
