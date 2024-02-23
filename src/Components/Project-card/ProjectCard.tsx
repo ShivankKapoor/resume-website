6eb3ed88740392260752ad14404daf59c9b2c86c
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2024-02-23 16:53:12 -0600

    Fix fonts and project

--- src/Components/Project-card/ProjectCard.tsx
+++ src/Components/Project-card/ProjectCard.tsx
@@ -1,21 +1,29 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-
+import project from "./project.png";
 import "./ProjectCard.css";
 
 function ProjectCard() {
   return (
-    <Card style={{ width: "90vw" }}>
-      <Card.Header>Project Name</Card.Header>
+    <Card style={{ width: "90vw" }} className="project-card">
+      <Card.Header className="project-name">
+        <h3>Project Name</h3>
+      </Card.Header>
       <Card.Body>
-        <div className="education-image-container">
-          <img className="education-image" src=" " alt="Education Icon" />
+        <div className="project-content-container">
+          <div className="project-image-container">
+            <img className="project-image" src={project} alt="Education Icon" />
+          </div>
+          <div className="project-info-container">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+              odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
+              quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
+              mauris. Fusce nec tellus sed augue semper porta.
+            </p>
+            <Button className="show-code-button" variant="primary">Go somewhere</Button>
+          </div>
         </div>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
   );
