9234c15f5e8c6cf9ff98f8221b3fc113b2a62013
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2024-03-23 15:11:15 -0500

    Add new project and resume

--- src/Pages/Projects/Projects.tsx
+++ src/Pages/Projects/Projects.tsx
@@ -4,12 +4,18 @@ import "./Projects.css";
 import projectImage from "./project.png";
 import reelRater from "./popcorn.png"
 import timeToLeave from "./clock.png"
-import droneCase from "./drone.png"
+import GPA from "./GPA.png"
 
 function Projects() {
   return (
     <>
       <div className="projects-container">
+      <ProjectCard
+          projectName="GPA-Analytics"
+          projectDesc="Developed a full stack grade-tracking application with an Angular Frontend and a Flask backend. This application allows users to visually track their GPA and do a 'what if' analysis to see how their GPA would be affected if they got a specific grade in a particular class."
+          gitHubLink="https://github.com/ShivankKapoor/GPA-Analytics"
+          projectIcon={GPA}
+        />
         <ProjectCard
           projectName="Reel-Rater"
           projectDesc="Created a movie rating application using Angular, Angular Material, and Pocketbase. This project allows users to rate and track their favorite movies, featuring a user-friendly interface with Angular Material components. The project uses a pocketbase backend that uses SQL and holds up relational database standards."
@@ -21,12 +27,6 @@ function Projects() {
           projectDesc="Built an application that sends a notification to the user to let them know when it is time to leave for their next class by calculating the walking time between the building they are currently at to the building they need to be in. Used java's swing toolkit to make the user interface and implemented the Google distance matrix API to calculate walking times."
           gitHubLink="https://github.com/ShivankKapoor/Time-To-Leave-Final"
           projectIcon={timeToLeave}
-        />
-        <ProjectCard
-          projectName="Drone Cast"
-          projectDesc="Developed a Python application that lets users know if it is safe to fly their drone. The front end was built using the Tkinter toolkit. Implements the Open Weather API to get weather conditions. Also Implements the IP-API to auto detect location."
-          gitHubLink="https://github.com/ShivankKapoor/Drone-Cast"
-          projectIcon={droneCase}
         />
       </div>
       <div className="subFooterDiv">
