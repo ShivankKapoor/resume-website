5a3b2def5ec0ccd906ace2213a69aa6a1e1a68dc
shivank <shivankkapoor@outlook.com>
2026-07-30 02:45:28 -0500

    Add animation when navigating to new page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
+import { useRef } from "react";
 import "./App.css";
 import Navibar from "./Components/Navibar/Navibar";
 import Home from "./Pages/Home/Home";
-import { BrowserRouter as Router, Route, Routes } from 'react-router';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router';
 import Projects from "./Pages/Projects/Projects";
 import Resume from "./Pages/Resume/Resume";
 import NotFound from "./Pages/404/NotFound";
 import ScrollToTop from "./Services/ScrollToTop";
 import usePageTracker from "./Hooks/usePageTracker";
+import { SwitchTransition, CSSTransition } from "react-transition-group";
 
 function PageTrackerWrapper() {
   usePageTracker(); // Use the page tracker hook here
   return null;
+}
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  const nodeRef = useRef(null);
+
+  return (
+    <SwitchTransition>
+      <CSSTransition
+        key={location.pathname}
+        nodeRef={nodeRef}
+        classNames="page-fade"
+        timeout={90}
+      >
+        <div ref={nodeRef}>
+          <Routes location={location}>
+            <Route path='/' element={<Home />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/resume' element={<Resume />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </div>
+      </CSSTransition>
+    </SwitchTransition>
+  );
 }
 
 function App() {
@@ -21,12 +48,7 @@ function App() {
       <div>
         <ScrollToTop />
         <Navibar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/projects' element={<Projects />} />
-          <Route path='/resume' element={<Resume />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
+        <AnimatedRoutes />
       </div>
     </Router>
   );
